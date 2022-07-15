d9ba84a474ec8e08e2d0edab672100130c5e2b38
mr_kim <rofalfl42@gmail.com>
2022-07-15 22:14:16 +0900

    **프로젝트** : Java Bluetooth Communication Client (feat.BluetoothSocket) **작성자** : illis **이슈**: **수정내역**: 1. BluetoothScanmodeReceiver.java: 블루투스가 검색 가능한 상태인지 확인하는 리시버 추가 2. BluetoothSearchReceiver.java: 블루투스 검색이 시작되면 컨트롤러와 연동하여 기기를 추가하기 위한 리시버 추가 3. BluetoothStateReceiver.java: 블루투스 연결 on/off 상태를 감지하기 위한 리시버 추가 4. MainActivity.java: 이벤트 수신을 위한 인텐트 필터 추가 및 권한 체크

--- JavaBtCommunicationClient/app/src/main/java/com/illis/javabtcommunicationclient/MainActivity.java
+++ JavaBtCommunicationClient/app/src/main/java/com/illis/javabtcommunicationclient/MainActivity.java
@@ -1,14 +1,61 @@
 package com.illis.javabtcommunicationclient;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.Context;
+import android.content.IntentFilter;
+import android.content.pm.PackageManager;
+import android.location.LocationManager;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    private LocationManager locationManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.BLUETOOTH_CONNECT) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+
+        locationManager = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
+        if(!locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+            // todo 위치 권한 팝업 노출
+            return;
+        }
+
+        //블루투스 브로드캐스트 리시버 등록
+        //리시버1
+        IntentFilter stateFilter = new IntentFilter();
+        //BluetoothAdapter.ACTION_STATE_CHANGED : 블루투스 상태변화 액션
+        stateFilter.addAction(BluetoothAdapter.ACTION_STATE_CHANGED);
+        registerReceiver(new BluetoothStateReceiver(), stateFilter);
+        //리시버2
+        IntentFilter searchFilter = new IntentFilter();
+        //BluetoothAdapter.ACTION_DISCOVERY_STARTED : 블루투스 검색 시작
+        searchFilter.addAction(BluetoothAdapter.ACTION_DISCOVERY_STARTED);
+        //BluetoothDevice.ACTION_FOUND : 블루투스 디바이스 찾음
+        searchFilter.addAction(BluetoothDevice.ACTION_FOUND);
+        //BluetoothAdapter.ACTION_DISCOVERY_FINISHED : 블루투스 검색 종료
+        searchFilter.addAction(BluetoothAdapter.ACTION_DISCOVERY_FINISHED);
+        searchFilter.addAction(BluetoothDevice.ACTION_BOND_STATE_CHANGED);
+        registerReceiver(new BluetoothSearchReceiver(), searchFilter);
+        //리시버3
+        IntentFilter scanmodeFilter = new IntentFilter();
+        scanmodeFilter.addAction(BluetoothAdapter.ACTION_SCAN_MODE_CHANGED);
+        registerReceiver(new BluetoothScanmodeReceiver(), scanmodeFilter);
     }
 }
