cba88ca930ec64261c2a873f126c22f6442cbfa0
mr_kim <rofalfl42@gmail.com>
2022-07-17 23:29:40 +0900

    **프로젝트** : Java Bluetooth Communication Client (feat.BluetoothSocket) **작성자** : illis **이슈**: **수정내역**: 1. MainActivity.java: 권한 체크 로직 수정(os 12에서 추가된 scan, connect 권한 체크 & 위치 권한 체크) 2. AndroidManifest.xml: 블루투스 관련 권한 추가

--- JavaBtCommunicationClient/app/src/main/java/com/illis/javabtcommunicationclient/MainActivity.java
+++ JavaBtCommunicationClient/app/src/main/java/com/illis/javabtcommunicationclient/MainActivity.java
@@ -10,30 +10,37 @@ import android.content.Context;
 import android.content.IntentFilter;
 import android.content.pm.PackageManager;
 import android.location.LocationManager;
+import android.os.Build;
 import android.os.Bundle;
 
+import com.illis.javabtcommunicationclient.receiver.BluetoothScanmodeReceiver;
+import com.illis.javabtcommunicationclient.receiver.BluetoothSearchReceiver;
+import com.illis.javabtcommunicationclient.receiver.BluetoothStateReceiver;
+
 public class MainActivity extends AppCompatActivity {
-    private LocationManager locationManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // check bt permission
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.BLUETOOTH_CONNECT) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
-            return;
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S)
+            {
+                ActivityCompat.requestPermissions(MainActivity.this,
+                        new String[]{Manifest.permission.BLUETOOTH_CONNECT,
+                                Manifest.permission.BLUETOOTH_SCAN}, 2);
+                return;
+            }
         }
 
-        locationManager = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
+        // check location permission
+        LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
         if(!locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
-            // todo 위치 권한 팝업 노출
+            ActivityCompat.requestPermissions(MainActivity.this,
+                    new String[]{Manifest.permission.ACCESS_COARSE_LOCATION,
+                            Manifest.permission.ACCESS_FINE_LOCATION}, 2);
             return;
         }
 
@@ -43,6 +50,7 @@ public class MainActivity extends AppCompatActivity {
         //BluetoothAdapter.ACTION_STATE_CHANGED : 블루투스 상태변화 액션
         stateFilter.addAction(BluetoothAdapter.ACTION_STATE_CHANGED);
         registerReceiver(new BluetoothStateReceiver(), stateFilter);
+
         //리시버2
         IntentFilter searchFilter = new IntentFilter();
         //BluetoothAdapter.ACTION_DISCOVERY_STARTED : 블루투스 검색 시작
@@ -53,6 +61,7 @@ public class MainActivity extends AppCompatActivity {
         searchFilter.addAction(BluetoothAdapter.ACTION_DISCOVERY_FINISHED);
         searchFilter.addAction(BluetoothDevice.ACTION_BOND_STATE_CHANGED);
         registerReceiver(new BluetoothSearchReceiver(), searchFilter);
+
         //리시버3
         IntentFilter scanmodeFilter = new IntentFilter();
         scanmodeFilter.addAction(BluetoothAdapter.ACTION_SCAN_MODE_CHANGED);
