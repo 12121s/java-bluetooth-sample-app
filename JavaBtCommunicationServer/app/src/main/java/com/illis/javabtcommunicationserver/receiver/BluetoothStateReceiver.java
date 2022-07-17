6afec211df624ff4225823c7a267e4398028b217
mr_kim <rofalfl42@gmail.com>
2022-07-17 23:24:18 +0900

    **프로젝트** : Java Bluetooth Communication Server (feat.Bluetooth Server Socket) **작성자** : illis **이슈**: **수정내역**: 1. BluetoothReceiver -> BluetoothStateReceiver.java: 블루투스 연결 이벤트를 받으면 블루투스 컨트롤러 실행(블루투스 listen, 서버 소켓 생성) 2. AndroidManifest.xml: 블루투스 관련 권한 추가

---
+++ JavaBtCommunicationServer/app/src/main/java/com/illis/javabtcommunicationserver/receiver/BluetoothStateReceiver.java
@@ -0,0 +1,47 @@
+package com.illis.javabtcommunicationserver.receiver;
+
+import android.annotation.SuppressLint;
+import android.bluetooth.BluetoothA2dp;
+import android.bluetooth.BluetoothDevice;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+import com.illis.javabtcommunicationserver.BluetoothController;
+import com.orhanobut.logger.Logger;
+
+public class BluetoothStateReceiver extends BroadcastReceiver {
+    private BluetoothController mBtController;
+    
+    @SuppressLint("MissingPermission")
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Logger.d("BluetoothA2dp = " + intent.getAction());
+        mBtController = BluetoothController.getInstance();
+
+        // paired
+        if(intent.getAction() == BluetoothDevice.ACTION_BOND_STATE_CHANGED && mBtController != null) {
+            mBtController.mDevice = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+            Logger.d("ACTION_BOND_STATE_CHANGED =" + mBtController.mDevice.getBondState());
+
+            if (mBtController.mDevice.getBondState() == BluetoothDevice.BOND_BONDED)
+                mBtController.WaitToSelectedDevice();
+        }
+        // connected
+        else if(intent.getAction() == BluetoothDevice.ACTION_ACL_CONNECTED && mBtController != null) {
+            mBtController.mDevice = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+            Logger.d("ACTION_BOND_STATE_CHANGED =" + mBtController.mDevice.getBondState());
+
+            if (mBtController.mDevice.getBondState() == BluetoothDevice.BOND_BONDED)
+                mBtController.WaitToSelectedDevice();
+        }
+        // disconnected
+        else if (intent.getAction() == BluetoothDevice.ACTION_ACL_DISCONNECTED) {
+            mBtController.mDevice = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+            Logger.d("ACTION_BOND_STATE_CHANGED =" + mBtController.mDevice.getBondState());
+
+            if (mBtController.mDevice.getBondState() == BluetoothDevice.BOND_NONE)
+                mBtController.disconnectBt();
+        }
+    }
+}
