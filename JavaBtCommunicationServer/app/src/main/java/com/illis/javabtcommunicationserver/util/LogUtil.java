1a9b96acc194891e28d00bf8c901dc8a727c3ea7
mr_kim <rofalfl42@gmail.com>
2022-07-18 23:22:00 +0900

    **프로젝트** : Java Bluetooth Communication Server (feat.Bluetooth Server Socket) **작성자** : illis **이슈**: obj가 null인 경우 fatal. **수정내역**: 1. null 체크 추가

--- JavaBtCommunicationServer/app/src/main/java/com/illis/javabtcommunicationserver/util/LogUtil.java
+++ JavaBtCommunicationServer/app/src/main/java/com/illis/javabtcommunicationserver/util/LogUtil.java
@@ -5,7 +5,8 @@ import com.orhanobut.logger.Logger;
 public class LogUtil
 {
     public static void logForCheckMemoryAddressAndHashCode(String message, Object obj) {
-        Logger.i(message + "-> hashCode = " + obj.hashCode() + "\n"
+        if (obj != null)
+            Logger.i(message + "-> hashCode = " + obj.hashCode() + "\n"
                     + message + "-> System.identityHashCode = " + System.identityHashCode(obj));
         OrderOfObjectsAfterGCMain.printAddresses(message + "-> Memory Address =", obj);
     }
